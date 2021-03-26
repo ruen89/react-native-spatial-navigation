611e90b46174b3fdac1382424215e19362545378
Ruen <ruen_offerman@hotmail.com>
2021-03-26 18:23:19 +0100

    Delete default examples. Add own example

---
+++ src/examples/Box.tsx
@@ -0,0 +1,78 @@
+/* Dependencies
+================================================================== */
+import React, { useCallback, useContext, useState } from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+import {
+  SpatialApi,
+  SpatialButton,
+  SpatialNavigationGroupContext,
+} from '../lib/SpatialNavigation'
+
+/* Types
+================================================================== */
+interface BoxProps {
+  hasTVPreferredFocus: boolean
+  index: number
+  onPress?: (index: number) => void
+  totalCount: number
+}
+
+/* Box
+================================================================== */
+export default function ({
+  hasTVPreferredFocus,
+  index,
+  totalCount,
+  onPress,
+}: BoxProps) {
+  const [isFocused, setFocused] = useState<boolean>(false)
+  const { groupId } = useContext(SpatialNavigationGroupContext)
+  const idPrefix = `${groupId}-Box-`
+  const id = `${idPrefix}${index}`
+
+  const handleFocusChange = useCallback((isButtonFocus: boolean) => {
+    setFocused(isButtonFocus)
+  }, [])
+
+  const handlePress = useCallback(() => {
+    const reverseIndex = totalCount - 1 - index
+    SpatialApi.setFocusToElement(`${idPrefix}${reverseIndex}`)
+  }, [idPrefix, index, totalCount])
+
+  return (
+    <SpatialButton
+      hasTVPreferredFocus={hasTVPreferredFocus}
+      id={id}
+      onFocusChange={handleFocusChange}
+      onPress={onPress || handlePress}
+      style={styles.boxRoot}
+    >
+      <View style={[styles.boxContent, isFocused && styles.focus]}>
+        <Text style={styles[isFocused ? 'focusText' : 'text']}>{index}</Text>
+      </View>
+    </SpatialButton>
+  )
+}
+
+const styles = StyleSheet.create({
+  boxRoot: {
+    marginHorizontal: 20,
+  },
+  boxContent: {
+    alignItems: 'center',
+    backgroundColor: 'rgba(250,250, 250, 0.6)',
+    height: 150,
+    justifyContent: 'center',
+    width: 150,
+  },
+  focus: {
+    backgroundColor: 'rgba(0,0,0, 0.2)',
+  },
+  text: {
+    color: '#242424',
+  },
+  focusText: {
+    color: '#fff',
+  },
+})
